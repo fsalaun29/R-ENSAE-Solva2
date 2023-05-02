9c1417a563de814fb3b9ea97553626284b478186
fsalaun29 <81226000+fsalaun29@users.noreply.github.com>
2023-05-02 16:02:26 +0200

    Add files via upload

--- C/rsum.c
+++ C/rsum.c
@@ -9,3 +9,16 @@ void rsum(double * N1, int * n, double * mu , double* sigma , double * S){
 	}
 	PutRNGstate();
 }
+
+void rsum2(double * N2, int * n, double * k , double * s, double * xi, double * S){
+	GetRNGstate();
+	for(int i = 0; i != *n ; ++i)
+	{
+		for(int j = 0; j != N2[i] ; j++)
+		{
+			float puiss = powf(runif(0,1), (*xi));
+			S[i] += (*k) + (*s)*(1/puiss - 1)/(*xi);
+		}
+	}
+	PutRNGstate();
+}
